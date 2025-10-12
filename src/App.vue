8cbee8c73f18bf614d39b846bf1215572ec4d939
kui2331 <157229009+kui2331@users.noreply.github.com>
2025-10-12 18:59:22 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { useLoginUserStore } from '@/stores/user.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+</script>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"> by kui233 </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+#basicLayout .content {
+  padding: 16px;
+  min-height: calc(100vh - 64px);
+  background: #fafafa;
+}
+#basicLayout .header {
+  background: #fff;
+}
+</style>
